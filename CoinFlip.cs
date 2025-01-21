679a27f30e8797dbd1f1723a8111b71b33bfec83
WesleyCaldwell <156111146+WesleyCaldwell@users.noreply.github.com>
2025-01-21 09:08:50 -0500

    Add files via upload

---
+++ CoinFlip.cs
@@ -0,0 +1,87 @@
+﻿using CulmProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CulmProject
+{
+    class CoinFlip
+    {
+        Random rand = new Random();
+        public int cf;
+        public int randPercent;
+        public int coinNum;
+        public int chance;
+        public int reading;
+        public int temp;
+        public int heads;
+        public int tails;
+
+        public int[] flipResults;
+        public CoinFlip()
+        {
+            cf = 0;
+            randPercent = 0;
+            chance = 0;
+            reading = 0;
+            temp = 0;
+            heads = 0;
+            tails = 0;
+        }
+
+        public void Flip()
+        {
+            cf = rand.Next(1, 3);
+        }
+
+
+
+        public void coins()
+        {
+            for(int i = 0; i < coinNum; i++)
+            {
+                Flip();
+                Array.Resize(ref flipResults, coinNum);
+                flipResults[i] = cf;
+            }
+        }
+
+        public void RandomFlip()
+        {
+            for(int i = 0;i < coinNum; i++)
+            {
+                if(flipResults[i] == 1)
+                {
+                    tails++;
+                }
+                else if (flipResults[i] == 2)
+                {
+                    heads++;
+                }
+            }
+        }
+
+        public void ManipulatePercent()
+        {
+            for(int i = 0;i < coinNum; i++)
+            {
+                randPercent = rand.Next(1, 101);
+
+                if(randPercent <= chance)
+                {
+                    flipResults[i] = 2;
+                    heads++;
+                }
+                else if(randPercent > chance)
+                {
+                    flipResults[i] = 1;
+                    tails++;
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics.PerformanceData;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,15 +14,20 @@ using System.Windows.Shapes;
 
 namespace CulmProject
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
-        Random rand = new Random();
+        CoinFlip coinflip; //instantiate class 
+
+
+
         public MainWindow()
         {
-            InitializeComponent();
-            MainVisible();
+            InitializeComponent(); // setting visibility  
+            MainVisible(); 
             FlipPageHidden();
             EndHidden();
+            CoinNumHidden();
+            SliderHidden();
             cmbResult.Visibility = Visibility.Hidden;
             imgGoldHeads.Visibility = Visibility.Hidden;
             imgGoldTails.Visibility = Visibility.Hidden;
@@ -27,139 +35,178 @@ namespace CulmProject
             ChoiceHidden();
             lblManipulatedTitle.Visibility = Visibility.Hidden;
             lblRandomTitle.Visibility = Visibility.Hidden;
+            cmbFlip.Visibility = Visibility.Hidden;
+            imgBackground.Visibility = Visibility.Hidden;
+            lblPercent.Visibility = Visibility.Hidden;
+            lblHeadsorTails.Visibility = Visibility.Hidden;
+            lblTotalFlipped.Visibility = Visibility.Hidden;
+
+
+            coinflip = new CoinFlip(); // instantiate coinflip 
+
 
         }
 
         private void cmbStart_Click(object sender, RoutedEventArgs e)
         {
-            MainHidden();
-            ChoiceVisible();
+            MainHidden(); // setting visibility  when start button is pressed 
+            CoinNumVisible();
+            imgBackground.Visibility = Visibility.Visible;
+        }
+        private void cmbConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+            int input = Convert.ToInt32(txtInput.Text); // getting user input for the amount of coins 
+            coinflip.coinNum = input; // setting that input to coinNum
             
+            CoinNumHidden(); // setting visibility  
+            ChoiceVisible();
         }
 
         private void rbHeads_Checked(object sender, RoutedEventArgs e)
         {
-            aniFlip.setMode(Animation.Modes.CoinFlip);
+            aniFlip.setMode(Animation.Modes.CoinFlip);// Making the animation work when you click heads or  tails 
         }
         private void rbTails_Checked(object sender, RoutedEventArgs e)
         {
-            aniFlip.setMode(Animation.Modes.CoinFlip);
+            aniFlip.setMode(Animation.Modes.CoinFlip);// Making the animation work when you click heads or  tails
         }
 
         private void rbManipulate_Checked(object sender, RoutedEventArgs e)
         {
-            ChoiceHidden();
-            FlipPageVisible();
-            lblManipulatedTitle.Visibility= Visibility.Visible;
+            ChoiceHidden(); // setting visibility  
+            cmbFlip.Visibility = Visibility.Visible;
+            lblPercent.Visibility= Visibility.Visible;
+            SliderVisible();
+            lblSlider.Content = "0%"; // setting default slider lable to 0% 
         }
         private void rbRandom_Checked(object sender, RoutedEventArgs e)
         {
-            ChoiceHidden();
+            ChoiceHidden(); // setting visibility  
             FlipPageVisible();
             lblRandomTitle.Visibility= Visibility.Visible;
+        }
+
+        private void slChance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            coinflip.chance = Convert.ToInt32(slChance.Value); // setting the coinflip.chance to the integer value of the slider 
+            lblSlider.Content = $"{coinflip.chance}%";// displayng that percent 
         }
 
 
         private void cmbFlip_Click(object sender, RoutedEventArgs e)
         {
+            lblPercent.Visibility = Visibility.Hidden; // setting visibility  
             ViewResult();
-            aniFlip.Initiate(10, "GoldFlip", 75);
-            lblManipulatedTitle.Visibility = Visibility.Hidden;
+            aniFlip.Initiate(10, "GoldFlip", 75); // instaniate the animation 
+            lblManipulatedTitle.Visibility = Visibility.Hidden; // setting visibility  
             lblRandomTitle.Visibility = Visibility.Hidden;
             rbTails.Visibility = Visibility.Hidden;
             rbHeads.Visibility = Visibility.Hidden;
-        }
+            SliderHidden();
+            coinflip.coins(); // calling coins function  
+         }
 
         private void cmbResult_Click(object sender, RoutedEventArgs e)
         {
+            if (rbManipulate.IsChecked == true)  // checking user input and doing an action based on that 
+            {
+                ManipulateResult();
+                EndHidden();
+            }
+            else if (rbRandom.IsChecked == true)
+            {
+                EndVisible();
+                RandomResult();        
+            }
             FlipPageHidden();
-            EndVisible();
-            aniFlip.Visibility = Visibility.Hidden;
-            RandomResult();
-
-            if (rbRandom.IsChecked == true)
-            {
-                RandomResult();
-            }
-            else if(rbManipulate.IsChecked == true)
-            {
-                if (rbHeads.IsChecked == true)
-                {
-                    imgGoldHeads.Visibility = Visibility.Visible;
-                    lblResult.Content = "It was Heads!";
-                    lblWinOrLose.Visibility = Visibility.Hidden;
-                }
-                else if (rbTails.IsChecked == true)
-                {
-                    imgGoldTails.Visibility = Visibility.Visible;
-                    lblResult.Content = "It was Tails!";
-                    lblWinOrLose.Visibility = Visibility.Hidden;
-                }
-            }
+            aniFlip.Visibility = Visibility.Hidden; // seting visibility 
+            lblPercent.Visibility = Visibility.Hidden;
+            lblTotalFlipped.Visibility = Visibility.Visible;
+            lblHeadsorTails.Visibility = Visibility.Visible;
+            lblTotalFlipped.Content = $"You flipped {coinflip.flipResults.Length} coins!"; // displaying how many coins you have inputed 
         }
 
+        public void ManipulateResult()
+        {
+            coinflip.ManipulatePercent();
+            lblHeadsorTails.Content = $"{coinflip.tails} landed on tails : {coinflip.heads} landed on heads"; // displaying results of coins 
 
+            if(coinflip.tails >= coinflip.heads) // displaying majority coins 
+            {
+                imgGoldTails.Visibility = Visibility.Visible;
+            }
+            else if(coinflip.heads > coinflip.tails)
+            {
+                imgGoldHeads.Visibility = Visibility.Visible;
+            }
+        }
 
         public void RandomResult()
         {
-
-            int cf = rand.Next(1, 3);
-
+            coinflip.coins();
             //1 is tails and 2 is heads
-            //make this appear on the endscreen
+            
+            coinflip.RandomFlip();
+            lblHeadsorTails.Content = $"{coinflip.tails} landed on tails : {coinflip.heads} landed on heads"; // displaying majority coins 
 
-            if (cf == 1 && rbTails.IsChecked == true)
+            if (coinflip.tails >= coinflip.heads && rbTails.IsChecked == true) // making a case for each possible outcome 
             {
-                lblWinOrLose.Content = "You Win";
                 imgGoldTails.Visibility = Visibility.Visible;
-                lblResult.Content = "It was Tails!";
-                lblResult.Visibility = Visibility.Visible;
+                lblWinOrLose.Content = "You Were Correct"; 
             }
-            else if (cf == 2 && rbTails.IsChecked == true)
+            else if (coinflip.heads >= coinflip.tails && rbTails.IsChecked == true)
             {
-                lblWinOrLose.Content = "You Lose";
                 imgGoldHeads.Visibility = Visibility.Visible;
-                lblResult.Content = "It was Heads!";
-                lblResult.Visibility = Visibility.Visible;
+                lblWinOrLose.Content = "You Were Wrong";
             }
-            else if (cf == 1 && rbHeads.IsChecked == true)
+            else if (coinflip.tails >= coinflip.heads && rbHeads.IsChecked == true)
             {
-                lblWinOrLose.Content = "You Lose";
                 imgGoldTails.Visibility = Visibility.Visible;
-                lblResult.Content = "It was Tails!";
-                lblResult.Visibility = Visibility.Visible;
+                lblWinOrLose.Content = "You Were Wrong";
             }
-            else if (cf == 2 && rbHeads.IsChecked == true)
+            else if (coinflip.heads >= coinflip.tails && rbHeads.IsChecked == true)
             {
-                lblWinOrLose.Content = "You Win";
                 imgGoldHeads.Visibility = Visibility.Visible;
-                lblResult.Content = "It was Heads!";
-                lblResult.Visibility = Visibility.Visible;
+                lblWinOrLose.Content = "You Were Correct";
+            }
+            else if(coinflip.heads >= coinflip.tails && rbHeads.IsChecked == false)
+            {
+                imgGoldHeads.Visibility = Visibility.Visible;
+                lblWinOrLose.Visibility = Visibility.Hidden;
+            }
+            else if(coinflip.tails >= coinflip.heads && rbTails.IsChecked == false)
+            {
+                imgGoldTails.Visibility = Visibility.Visible;
+                lblWinOrLose.Visibility = Visibility.Hidden;
             }
         }
 
+
+        //the following are functions to set visibilities:
+
         public void MainVisible()
         {
-            cmbStart.Visibility = Visibility.Visible;
+            cmbStart.Visibility = Visibility.Visible; //setting visibility
             imgTitle.Visibility = Visibility.Visible;
         }
 
         public void MainHidden()
         {
-            cmbStart.Visibility = Visibility.Hidden;
+            cmbStart.Visibility = Visibility.Hidden;//setting visibility
             imgTitle.Visibility = Visibility.Hidden;
         }
 
         public void FlipPageVisible()
         {
-            cmbFlip.Visibility = Visibility.Visible;
+            cmbFlip.Visibility = Visibility.Visible;//setting visibility
             rbHeads.Visibility = Visibility.Visible;
             rbTails.Visibility = Visibility.Visible;
         }
 
         public void FlipPageHidden()
         {
-            cmbFlip.Visibility = Visibility.Hidden;
+            cmbFlip.Visibility = Visibility.Hidden;//setting visibility
             cmbResult.Visibility = Visibility.Hidden;
             rbTails.Visibility = Visibility.Hidden;
             rbHeads.Visibility = Visibility.Hidden;
@@ -167,32 +214,55 @@ namespace CulmProject
 
         public void ViewResult()
         {
-            cmbResult.Visibility = Visibility.Visible;
+            cmbResult.Visibility = Visibility.Visible;//setting visibility
             cmbFlip.Visibility = Visibility.Hidden;
         }
 
         public void EndHidden()
         {
-            lblWinOrLose.Visibility = Visibility.Hidden;   
+            lblWinOrLose.Visibility = Visibility.Hidden;   //setting visibility
 
         }
 
         public void EndVisible()
         {
-            lblWinOrLose.Visibility = Visibility.Visible;
+            lblWinOrLose.Visibility = Visibility.Visible;//setting visibility
         }
 
         public void ChoiceHidden()
         {
-            rbManipulate.Visibility = Visibility.Hidden;
+            rbManipulate.Visibility = Visibility.Hidden;//setting visibility
             rbRandom.Visibility = Visibility.Hidden;
             lblChoice.Visibility = Visibility.Hidden;
         }
         public void ChoiceVisible()
         {
-            rbManipulate.Visibility = Visibility.Visible;
+            rbManipulate.Visibility = Visibility.Visible;//setting visibility
             rbRandom.Visibility = Visibility.Visible;
             lblChoice.Visibility = Visibility.Visible;
+        }
+        public void CoinNumVisible()
+        {
+            txtInput.Visibility = Visibility.Visible;//setting visibility
+            lblManipulatedTitle.Visibility = Visibility.Visible;
+            cmbConfirm.Visibility = Visibility.Visible;
+        }
+        public void CoinNumHidden()
+        {
+            txtInput.Visibility = Visibility.Hidden;//setting visibility
+            lblManipulatedTitle.Visibility = Visibility.Hidden;
+            cmbConfirm.Visibility = Visibility.Hidden;
+        }
+
+        public void SliderVisible()
+        {
+            slChance.Visibility = Visibility.Visible;//setting visibility
+            lblSlider.Visibility = Visibility.Visible;
+        }
+        public void SliderHidden()
+        {
+            slChance.Visibility = Visibility.Hidden;//setting visibility
+            lblSlider.Visibility = Visibility.Hidden;
         }
     }
 }
